36ba00ffe2436711945cd0815a07d34ef70db650
Jacob Bassett <bassettpro1@gmail.com>
2019-05-08 13:08:50 -0400

    morning day 3

---
+++ 05-expressionsAndOperators/03-ternaries.js
@@ -0,0 +1,16 @@
+//ternaries
+
+//ternaries are a shorthand version of an if else if statemet
+
+//conditional statement
+
+
+//ternary
+(x > 0) ? console.log('yes') : console.log('no');
+
+// 
+
+let age = 15;
+(age >= 25) ? console.log('Yay! You can rent a car!') : (age >= 21) ? console.log('Yay! You can drink!') ? (age >= 18) : console.log('Yay! you can vote!') : console.log('sorry, you are too young to do anything fun');
+
+(yep < 0 && yep > -10) ? console.log('it works') :(yep > 0) ? console.log('it works') : console.log ('didn/'t work');
